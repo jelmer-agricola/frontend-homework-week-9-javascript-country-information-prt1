2d9120e1fe894668f06bd80c768a51f6a31af98b
jelmer-agricola <jelmer.agricola@gmail.com>
2022-11-09 10:05:55 +0100

    opdracht 1 zo goed als afgerond opzet voor opdracht 2 gemaakt

--- src/app.js
+++ src/app.js
@@ -1,3 +1,88 @@
+// https://github.com/jelmer-agricola/frontend-homework-week-9-javascript-country-information-prt1/pull/1
+import axios from 'axios';
+
+async function fetchCountriesData() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+        const countries = result.data;
+        console.log(result);
+        console.log(result.data);
+
+//4.  los een land loggen Nederland in dit geval
+        console.log(result.data[157].population);
+// sort functie maken voor landen op populatie van laag naar hoog
+
+        countries.sort((a, b) => a.population - b.population);
 
 
-console.log('Hallo daar!');
+        generateCountriesList(countries);
+
+// array loggen met landnamen of iets anders.
+        // const newArray = [];
+        //
+        //  // for (let i = 0; i < result.data.length; i++) {
+        //  //     newArray.push(result.data[0].name);
+        //  // }
+        //  // console.log(newArray);
+
+        // console.log(countryName);
+    } catch (err) {
+        console.error(err);
+        const errorMessage = doucment.getElementById('error-message');
+        //    check welke error message van toepassing is
+        if (err.result.status === 404) {
+            errorMessage.textContent = "Page nog Found | 404"
+        }
+        if (err.result.status === 500) {
+            errorMessage.textContent = "Internal Service error | 500"
+        }
+    }
+}
+
+fetchCountriesData();
+
+function generateCountriesList(countryArray) {
+    const countryList = document.getElementById('country-list');
+    countryList.innerHTML = countryArray.map((country) => {
+        return `
+        <li>
+            <img class="flags" src="${country.flag}" alt="flag of the country"/>
+            <span class="${getRegionClass(country.region)}">${country.name}</span>    
+            <p>Has a population of ${country.population} people</p>
+        </li>
+        `;
+    }).join('');
+}
+
+// generateCountriesList();
+
+function getRegionClass(region) {
+    switch (region) {
+        /*# Africa: blauw*/
+        case 'Africa':
+            return 'blue';
+        /*# Americas: groen*/
+        case 'Americas':
+            return 'green';
+        /*# Asia: rood*/
+        case 'Asia':
+            return 'red';
+        /*# Europe: geel*/
+        case 'Europe':
+            return 'yellow';
+        /*# Oceania: paars*/
+        case 'Oceania':
+            return 'purple';
+        default:
+            return 'default';
+
+    }
+}
+
+
+// #De naam van het land
+// name: Afghanistan
+// #De vlag van dat land
+// Zit onder flag
+// #De zin: Has a population of [amount] people
+// zit onder population
